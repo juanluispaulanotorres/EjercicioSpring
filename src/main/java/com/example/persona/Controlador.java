9e894d3b2de02707febbf4c25a31df4386798ce7
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2022-02-11 12:29:21 +0100

    Obtener información de la persona

--- src/main/java/com/example/persona/Controlador.java
+++ src/main/java/com/example/persona/Controlador.java
@@ -2,6 +2,7 @@ package com.example.persona;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,11 @@ public class Controlador {
     public String getDatosPersona(@PathVariable String nombre) {
         Persona p1 = new Persona(nombre, "Jaén", 26);
         return "Hola " + p1.getNombre();
+    }
+
+    @GetMapping("/useradd")
+    public Persona postPersona() {
+        Persona p1 = new Persona("chuchi", "Logroño", 33 + 1);
+        return p1;
     }
 }
